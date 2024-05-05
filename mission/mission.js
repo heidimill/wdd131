fdc51464bc2c07d86791249c07b124c0b8e6a64a
heidimill <mil22070@byui.edu>
2024-05-04 19:05:38 -0600

    Mission part 2

---
+++ mission/mission.js
@@ -0,0 +1,27 @@
+
+const themeSelector = document.getElementById('theme'); 
+
+function changeTheme() {
+    // Check the current value of the select element
+    const selectedTheme = themeSelector.value;
+    
+    // Get references to body and logo elements
+    const body = document.body;
+    const logo = document.getElementById('logo'); 
+    
+    // Perform actions based on selected theme
+    if (selectedTheme === 'dark') {
+        // Add the dark class to the body
+        body.classList.add('dark');
+        // Change the source of the logo to point to the white logo
+        logo.src = './images/byui-logo_white.png'; 
+    } else {
+        // Remove the dark class from the body
+        body.classList.remove('dark');
+        // Make sure the logo src is the blue logo
+        logo.src = "./images/byui-logo_blue.webp"; 
+    }
+}
+
+// Add event listener to the themeSelector element
+themeSelector.addEventListener('change', changeTheme); // Use 'change' event instead of 'click' for select elements
